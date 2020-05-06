01f340a1b5f63b438c6c4b50ad9b79b258e71df7
Shaheer Ahmed <62714282+boringcuriosity@users.noreply.github.com>
2020-05-06 16:57:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const Unapp = new Vue({
 		getPosts: function() {
 			this.isloading = true;
 			window
-				.fetch('https://factera.unmasp.tech/factera.json')
+				.fetch('https://boringcuriosity.github.io/factera/factera.json')
 				.then(response => {
 					if (response.status >= 200 && response.status < 300) return response.json();
 				})
